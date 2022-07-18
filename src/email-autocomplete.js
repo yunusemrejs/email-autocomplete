b929c5a1bae05e2e0da6da5abcced2e760eb218c
Emre Kara <yunusemrejs@outlook.com>
2022-07-18 19:49:31 +0300

    convert prototype to es6 class syntax

--- src/email-autocomplete.js
+++ src/email-autocomplete.js
@@ -1,19 +1,16 @@
-(() => {
-
-  this.EmailAutocomplete = function (input, options) {
+class EmailAutocomplete {
+  constructor(input, options) {
     this.input = input;
     this.options = options;
     this.init();
   }
-
-  EmailAutocomplete.prototype.init = function () {
+  init() {
     this.createInputValueSizeSpan();
     this.createSuggestionSpan();
     this.input.addEventListener('keyup', this.onKeyUp.bind(this));
     this.input.addEventListener('blur', this.blur.bind(this));
   }
-
-  EmailAutocomplete.prototype.createInputValueSizeSpan = function () {
+  createInputValueSizeSpan() {
     const inputStyle = getComputedStyle(this.input);
     this.inputSizeSpan = document.createElement('span');
     this.inputSizeSpan.classList.add('email-autocomplete-input-size');
@@ -34,8 +31,7 @@
     }
     Object.assign(this.inputSizeSpan.style, inputSizeSpanStyles);
   }
-
-  EmailAutocomplete.prototype.createSuggestionSpan = function () {
+  createSuggestionSpan() {
     const inputStyle = getComputedStyle(this.input);
     this.suggestionSpan = document.createElement('span');
     this.suggestionSpan.classList.add('email-autocomplete-suggestion-span');
@@ -63,22 +59,23 @@
     this.suggestionSpan.addEventListener('click', this.onSuggestionItemClick.bind(this));
   }
 
-  EmailAutocomplete.prototype.getSuggestionFieldLeftOffset = function () {
+  getSuggestionFieldLeftOffset() {
     return this.inputSizeSpan.offsetWidth + 4;
   }
 
-
-  EmailAutocomplete.prototype.isEmail = function (email) {
-    if (email.indexOf('@') > -1) {
-      return true;
+  isEmail(email) {
+    if (email.indexOf('@') == -1) {
+      return false;
     }
+
+    return true;
   }
 
-  EmailAutocomplete.prototype.getDomain = function (email) {
+  getDomain(email) {
     return email.split('@')[1].toLowerCase();
   }
 
-  EmailAutocomplete.prototype.onKeyUp = function (e) {
+  onKeyUp() {
     this.suggestionSpan.style.display = 'none';
     this.hasSuggestion = false;
     this.suggestionSpan.innerHTML = '';
@@ -102,17 +99,17 @@
     // Get the email autocomplete suggestions
     this.getAutocompleteSuggestion(domain);
   }
-
-  EmailAutocomplete.prototype.getAutocompleteSuggestion = function (domain) {
+  getAutocompleteSuggestion(domain) {
     this.suggestionSpan.style.display = 'none';
     this.suggestionSpan.innerHTML = '';
 
     this.hasSuggestion = false;
 
-    this.options.autoCompleteList.forEach(autoCompleteListItem => {
+    this.options.autoCompleteList.some(autoCompleteListItem => {
       if (autoCompleteListItem.startsWith(domain)) {
         this.setSuggestionSpanValue(domain, autoCompleteListItem)
         this.hasSuggestion = true;
+        return true;
       }
     });
 
@@ -124,7 +121,7 @@
     this.setSuggestionSpanPosition();
   }
 
-  EmailAutocomplete.prototype.onSuggestionItemClick = function (e) {
+  onSuggestionItemClick(e) {
     if (value === null) {
       return;
     }
@@ -133,31 +130,29 @@
     this.confirmSuggestion(value);
   }
 
-  EmailAutocomplete.prototype.confirmSuggestion = function (value) {
+  confirmSuggestion(value) {
     let email = this.input.value.split('@')[0] + '@' + value;
 
     this.input.value = email;
 
     this.suggestionSpan.style.display = 'none';
     this.suggestionSpan.innerHTML = '';
-  };
+  }
 
-  EmailAutocomplete.prototype.setSuggestionSpanValue = function (domain, autoCompleteListItem) {
+  setSuggestionSpanValue(domain, autoCompleteListItem) {
     let suggestionText = autoCompleteListItem.substring(domain.length);
 
     this.suggestionSpan.setAttribute('data-value', autoCompleteListItem);
     this.suggestionSpan.innerHTML = suggestionText;
   }
 
-  EmailAutocomplete.prototype.setSuggestionSpanPosition = function () {
+  setSuggestionSpanPosition() {
     let topPosition = this.input.offsetHeight / 2 - this.suggestionSpan.offsetHeight / 2;
     this.suggestionSpan.style.transform = 'translateY(' + topPosition + 'px) translateX(' + this.getSuggestionFieldLeftOffset() + 'px)';
   }
-
-  EmailAutocomplete.prototype.blur = function () {
-
+  blur() {
     if (this.hasSuggestion == true) {
       this.confirmSuggestion(this.suggestionSpan.getAttribute('data-value'));
     }
   }
-})();
+}
